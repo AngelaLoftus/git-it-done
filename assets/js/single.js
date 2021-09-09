40a44f43a7833d63f530a5d1d22210b4e979f9de
Angela Loftus <angelaloftus@yahoo.com>
2021-09-09 13:27:02 -0500

    link issues to username

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,16 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -27,8 +38,6 @@ var getRepoIssues = function(repo) {
 
     
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -80,3 +89,5 @@ var displayWarning = function(repo) {
     //append to warning container 
     limitWarningEl.appendChild(linkEl);
 };
+
+getRepoName();
